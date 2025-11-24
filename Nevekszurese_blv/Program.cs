4536b245703e73931b70cd890690b40d2e239338
bakilv <bakilv@310-16>
2025-11-24 08:14:30 +0100

    Add project files.

---
+++ Nevekszurese_blv/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Nevekszurese_blv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
